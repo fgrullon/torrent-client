dc64fe061af285f58da62b81a51833232599aebd
fgrullon <franfelix@gmail.com>
2023-12-07 15:30:22 -0400

    add ben decode and add to index.js url, buffer, parse and dgram package as well as test using sockets the connection

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 "use strict";
 
 import fs from "fs";
-import bencode from "bencode";
+import decode from "./lib/bendecode.js";
+
+import url from "node:url";
+import dgram from "dgram";
+import { Buffer } from "buffer";
+import parse from "url-parse";
 
 /*
  *  Read torrent file from local storage and pass it to the decode function of bencode library
  */
-const torrent = bencode.decode(fs.readFileSync("puppy.torrent"));
+const torrent = decode(fs.readFileSync("puppy.torrent"));
 
-console.log(torrent.announce.toString("utf8"));
+const url2 = parse(torrent.announce.toString("utf8"));
+
+const socket = dgram.createSocket("udp4");
+const myMsg = Buffer.from("hello?", "utf8");
+
+socket.send(myMsg, 0, myMsg.length, url.port, url.host, () => {});
+
+socket.on("message", (msg) => {
+  console.log("message is ", msg);
+});
